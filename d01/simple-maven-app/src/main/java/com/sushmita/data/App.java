4cbcf1df76751287c694002b66e926d755e67af0
admin <susmita@email.com>
2023-09-01 00:00:11 -0700

    new project created

---
+++ d01/simple-maven-app/src/main/java/com/sushmita/data/App.java
@@ -0,0 +1,13 @@
+package com.sushmita.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Sushmita!" );
+    }
+}
